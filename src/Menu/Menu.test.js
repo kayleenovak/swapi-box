102a5b225c2169976f242c0f74ef022fd517eb26
Kaylee Bunce <kaybunce@hotmail.com>
2018-12-04 21:20:01 -0700

    Add snapshot test to Menu component

---
+++ src/Menu/Menu.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import Menu from './Menu'
+
+describe('Menu', () => {
+  it('should match the snapshot', () => {
+    const mockSelection = 'People'
+    const wrapper = shallow(<Menu currentSelection={ mockSelection } />)
+
+    expect(wrapper).toMatchSnapshot()
+  })
+})
