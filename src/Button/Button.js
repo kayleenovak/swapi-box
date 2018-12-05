055dc19ce930eb595c402854dfcda070f5574863
Kylie Stanley <knstanley11@gmail.com>
2018-12-05 14:08:41 -0700

    Fix menu

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
+
 const Button = (props) => {
   const selected = props.name === props.currentSelection ? 'selected' : ''
   return (
     <li class={`${selected} menu-item`}>
-          <a href="#menu">
-            <span>{ props.name }</span>
-          </a>
+          <a href="https://www.facebook.com/" target="_blank"> {props.name} </a>
     </li>
   )
 }
