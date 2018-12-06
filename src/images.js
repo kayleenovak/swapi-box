516920ce93f540f08c92bc3a134f132b38683e2a
Kylie Stanley <knstanley11@gmail.com>
2018-12-05 17:42:20 -0700

    Adjust menu style

--- src/images.js
+++ src/images.js
@@ -1,5 +1,14 @@
 const Images = {
-  'Luke Skywalker': '../images/Luke.png'
+  'Luke Skywalker': '../images/Luke.png',
+  "C-3PO":'../images/C3PO.png',
+  "R2-D2": '../images/R2D2.png',
+  "Darth Vader": '../images/Luke.png',
+  "Leia Organa": '../images/Luke.png',
+  "Owen Lars": '../images/Luke.png',
+  "Beru Whitesun lars": '../images/Luke.png',
+  "R5-D4": '../images/Luke.png',
+  "Biggs Darklighter": '../images/Luke.png',
+  "Obi-Wan Kenobi": '../images/Luke.png',
 }
 
 export default Images;
