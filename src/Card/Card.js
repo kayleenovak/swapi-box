a3c124dc3e48f30b3beb280c90d5349c975c200f
Kaylee Bunce <kaybunce@hotmail.com>
2018-12-06 16:11:51 -0700

    Add all info to cards

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -3,11 +3,18 @@ import './Card.scss'
 import Images from '../images.js'
 
 const Card = (props) => {
-  const image = 'Luke Skywalker'
+  const allInfo = Object.keys(props.prop).filter(key => {
+    return key !== 'name' && key !== 'favorite'
+  })
+
+  const info = allInfo.map(key => {
+    return <p>{ key }: { props.prop[key] }</p>
+  })
   return (
     <div className='card'>
       <h1 className='card-text'>{props.prop.name}</h1>
-      <img className='card-image' src={Images[image]} />
+      <img className='card-image' src={Images[props.prop.name]} />
+      { info }
     </div>
     )
 }
