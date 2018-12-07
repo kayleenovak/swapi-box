a2e638293a0178597bf415ef5b29ca00cdb7fa7f
Kylie Stanley <knstanley11@gmail.com>
2018-12-07 12:50:44 -0700

    Add active class styling

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import './Button.scss'
+import '../Menu/Menu.scss'
 
 
 const Button = (props) => {
   return (
     <li class='menu-item'>
-      <NavLink to={`/main/${props.name}`}>
+      <NavLink to={`/main/${props.name}`} className="nav-btn">
        {props.name} 
       </NavLink>
    </li>
