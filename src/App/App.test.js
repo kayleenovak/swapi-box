fe15418be784257fd4d3b1c86a273ffad3ba9d67
Kaylee Bunce <kaybunce@hotmail.com>
2018-12-08 16:20:53 -0700

    Add tests to App component

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, withRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom'
 import App from './App';
 import { shallow, mount } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(router, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(router, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
 
 describe('App', () => {
   it('should match the snapshot', () => {
@@ -17,23 +18,48 @@ describe('App', () => {
     expect(wrapper).toMatchSnapshot()
   })
 
-  it('should toggle the splash page when toggleSplash is invoked', () => {
-    const wrapper = shallow(
-      <Router>
-        <App />
-      </Router>)
+  describe('componentDidMount', () => {
+    let wrapper
+    let getPeople
+    let getVehicles
+    let getPlanets
 
-    const expectedState = {
-      'showSplash': false,
-      'films': [],
-      'currentSelection': 'People',
-      'people': [],
-      'vehicles': [],
-      'planets': []
-    }
+    beforeEach(async () => {
+      wrapper = shallow(<App />)
+      wrapper.people.fetchPeople = jest.fn()
+      wrapper.vehicles.fetchVehicles = jest.fn()
+      wrapper.planets.fetchPlanets = jest.fn()
+      getPeople = await wrapper.people.fetchPeople
+      getVehicles = await wrapper.vehicles.fetchVehicles
+      getPlanets = await wrapper.planets.fetchPlanets
+    })
 
-    wrapper.instance().toggleSplash()
-    
-    expect(wrapper.state()).toEqual(expectedState)
+    it('should fire fetchPeople', async () => {
+      await wrapper.instance().componentDidMount()
+
+      expect(getPeople).toHaveBeenCalled()
+    })
+
+    it('should fire fetchVehicles', async () => {
+      await wrapper.instance().componentDidMount()
+
+      expect(getVehicles).toHaveBeenCalled()
+    })
+
+    it('should fire fetchPlanets', async () => {
+      await wrapper.instance().componentDidMount()
+
+      expect(getPlanets).toHaveBeenCalled()
+    })
+
+    it('should fire getLocalStorage', () => {
+
+    })
+
+    it('should setState with the correct data', () => {
+
+    })
+
   })
+
 })
