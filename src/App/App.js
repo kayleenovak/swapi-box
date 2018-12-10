b7e7df9a6d464fc77a3eadc98b203c2fd1ac48d8
Kaylee Bunce <kaybunce@hotmail.com>
2018-12-10 13:03:30 -0700

    Fix linting errors

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import People from '../helper/People'
 import Vehicles from '../helper/Vehicles'
 import Planets from '../helper/Planets'
 import Splash from '../Splash/Splash'
 import Main from '../Main/Main'
-import { getLocalStorage, setLocalStorage } from '../helper/localStorage.js'
-import { Route, withRouter } from 'react-router-dom'
-import './App.scss';
+import { getLocalStorage, setLocalStorage } from '../helper/localStorage'
+import './App.scss'
 
 class App extends Component {
   constructor() {
@@ -20,9 +20,9 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    this.people = new People();
-    this.vehicles = new Vehicles();
-    this.planets = new Planets();
+    this.people = new People()
+    this.vehicles = new Vehicles()
+    this.planets = new Planets()
 
     this.setState({
       people: await this.people.fetchPeople(),
@@ -33,12 +33,12 @@ class App extends Component {
   }
 
   handleFavorite = (data, itemType) => {
-    let updatedItems;
-    let favorites;
+    let updatedItems
+    let favorites
 
     const found = this.state.favorites.find(item => item.name === data.name)
-    
-    if(!found) {
+
+    if (!found) {
       updatedItems = this.toggleItemState(data, itemType)
       data.favorite = true
       favorites = [...this.state.favorites, data]
@@ -58,22 +58,23 @@ class App extends Component {
   }
 
   toggleItemState = (data, itemType) => {
-    return this.state[itemType].map(item => {
+    return ( this.state[itemType].map(item => {
       if (item === data) {
         item.favorite = !item.favorite
       }
       return item
-    })
+      })
+    )
   }
 
   render() {
     return (
       <div>
-          <Route exact path='/' component={Splash} />
-          <Route path='/main' render= {({match}) => <Main {...this.state} handleFavorite={ this.handleFavorite }/>} /> 
+        <Route exact path="/" component={Splash} />
+        <Route path="/main" render={({ match }) => <Main {...this.state} handleFavorite={this.handleFavorite} />} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
