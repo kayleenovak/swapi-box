a188fa54e895347367b5394067fbba622ba1e72f
Kylie Stanley <knstanley11@gmail.com>
2018-12-11 16:48:37 -0700

    Finish film tests and linting

--- src/helper/Films.test.js
+++ src/helper/Films.test.js
@@ -6,6 +6,7 @@ describe('Films', () => {
   beforeEach(() => {
     films = new Films()
     mockUrl = 'https://swapi.co/api/films/'
+
   })
 
   it('should call fetchData with the correct params', async () => {
@@ -15,7 +16,20 @@ describe('Films', () => {
     expect(films.fetchData).toHaveBeenCalledWith(mockUrl)
   })
 
-  it('should return an array of films', () => {
+  it('should return an array of films', async () => {
+    const expected = 'https://swapi.co/api/films/' 
+    const mockData = {
+      characters: [],
+      created: "2014-12-10T14:23:31.880000Z",
+      director: "George Lucas",
+      edited:"2015-04-11T09:46:52.774897Z",
+      episode_id: 4,
+      opening_crawl: 'crawl',
+    }
 
+    films.fetchData = jest.fn().mockImplementation(() => Promise.resolve(mockData))
+    const newFilms = await films.fetchFilms()
+
+    expect(newFilms).toEqual(mockData)
   })
 })
