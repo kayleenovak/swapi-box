2c873c74d436a52cc44750c1e2a0767ade5c4362
Kylie Stanley <knstanley11@gmail.com>
2018-12-11 17:01:13 -0700

    Edit film tests

--- src/helper/Films.test.js
+++ src/helper/Films.test.js
@@ -6,7 +6,6 @@ describe('Films', () => {
   beforeEach(() => {
     films = new Films()
     mockUrl = 'https://swapi.co/api/films/'
-
   })
 
   it('should call fetchData with the correct params', async () => {
@@ -17,14 +16,13 @@ describe('Films', () => {
   })
 
   it('should return an array of films', async () => {
-    const expected = 'https://swapi.co/api/films/' 
     const mockData = {
       characters: [],
-      created: "2014-12-10T14:23:31.880000Z",
-      director: "George Lucas",
-      edited:"2015-04-11T09:46:52.774897Z",
+      created: '2014-12-10T14:23:31.880000Z',
+      director: 'George Lucas',
+      edited: '2015-04-11T09:46:52.774897Z',
       episode_id: 4,
-      opening_crawl: 'crawl',
+      opening_crawl: 'crawl'
     }
 
     films.fetchData = jest.fn().mockImplementation(() => Promise.resolve(mockData))
